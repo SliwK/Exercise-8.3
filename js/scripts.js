ea921a5ee705d8999c0f73f35bac3df36527a1a1
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2017-11-23 20:49:40 +0100

    Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 // scripts.js
 
 var name = prompt('Enter your name');
+/*
+if (name == 'Krzysztof') {
+    alert('I already know you.. take your bike!');
+} else {
+*/
 alert('Hello, ' + name);
+// }
 console.log('Hello, ' + name);
